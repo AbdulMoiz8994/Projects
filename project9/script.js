ad5eb1ee1eb0b20ada291daed25b10c5529c6315
AbdulMoiz8994 <moiza8994@gmail.com>
2021-12-06 17:38:00 +0500

    Add the calcultor via evil()function which accpet the string and menuplate

--- project9/script.js
+++ project9/script.js
@@ -1,32 +1,13 @@
-// get the dom elements
-// console.log(screen.value);
-let calcultor=false;
+const screen=document.querySelector('[data-input]')
+
+
+function getData(num){
+    if(num === '.' && screen.value.includes('.'))return;
+    screen.value +=num
+}
+function submitvalue(){
+    screen.value =eval(screen.value)
+}
 function clearData(){
-    const screen=document.getElementById('screen');
-   screen.value="";
-   calcultor=false
-}
-
-
-function getNumber(num){
-    // console.log(num);
-    const screen=document.getElementById('screen');
-     if(calcultor == true){
-         if(num == "+" || num == "-" || num == "/" || num == "*"){
-             screen.value +=num;
-         }else{
-         screen.value =num;
-         }
-     }else{
-         screen.value +=num
-     }
-     calcultor=false
-}
-
-
-function submit(){
-    const screen=document.getElementById('screen');
-    // console.log(screen.value);
-    screen.value=eval(screen.value)
-    calcultor=true
+    screen.value=""
 }
